169c2593072838fd70c275052bcae34ed3fa4d18
ntritin62 <21522677@gm.uit.edu.vn>
2024-03-15 18:15:34 +0700

    set up mongoose

--- app.mjs
+++ app.mjs
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'dotenv/config';
 
 const app = express();
 
@@ -6,4 +7,11 @@ app.get('/', (req, res) => {
   res.send('hello');
 });
 
-app.listen(8080);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.5sd0ofw.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`
+  )
+  .then((result) => {
+    app.listen(process.env.PORT || 8080);
+  })
+  .catch((err) => console.log(err));
