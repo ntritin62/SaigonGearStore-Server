283a4ead66ac270fb96207d30b965e850fb485cc
ntritin62 <21522677@gm.uit.edu.vn>
2024-03-15 18:18:19 +0700

    finish app.mjs

--- app.mjs
+++ app.mjs
@@ -1,10 +1,49 @@
 import express from 'express';
 import 'dotenv/config';
+import mongoose from 'mongoose';
 
 const app = express();
 
+const fileStorage = multer.memoryStorage({});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(bodyParser.json());
+
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('hello');
+});
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
 mongoose
