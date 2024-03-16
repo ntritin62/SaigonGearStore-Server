bd000f9cca5b34cee8d8fb9e6b57273d0f747551
ntritin62 <21522677@gm.uit.edu.vn>
2024-03-16 14:01:50 +0700

    set up cart schema

---
+++ models/cart.mjs
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const cartSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    products: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          required: true,
+          ref: 'Product',
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('Cart', cartSchema);
--- models/user.mjs
+++ models/user.mjs
@@ -2,25 +2,30 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  fullName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  address: [
-    {
+const userSchema = new Schema(
+  {
+    fullName: {
       type: String,
-      required: false,
+      required: true,
     },
-  ],
-});
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: [
+      {
+        type: String,
+        required: false,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model('User', userSchema);
